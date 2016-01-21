b721be17cd99768e51afc291a93254f7bfdd727f
ElTororo <rom.moya@ymail.com>
2016-01-21 13:21:54 +0100

    getV3

--- Composant1/Composant1.cpp
+++ Composant1/Composant1.cpp
@@ -1,6 +1,7 @@
-
+#include "Composant3.h"
 #include "Composant1.h"
 #include "Composant1Version.h"
+
 
 // declaration des fonctions internes
 int addition_interne(int a1, int a2);
@@ -9,7 +10,7 @@ int addition_interne(int a1, int a2);
 
 int composant1(int p1, int p2)
 {
-	return addition_interne(p1,p2);
+	return composant3(p1,p2);
 }
 
 int addition_interne(int a1, int a2)
@@ -19,5 +20,6 @@ int addition_interne(int a1, int a2)
 
 char * getComposant1Version()
 {
-	return "Composant 1 version " COMPOSANT_VERSION_STR;
+
+	return "Composant 1 version " COMPOSANT_VERSION_STR + getComposant2Version();
 }
